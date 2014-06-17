9fab79d102e50a3f757ea7ff83ff81ecfa389f00
rzlt2014 <fairy1292@gmail.com>
2014-06-17 12:31:45 -0700

    try Greedy

---
+++ LongestSubstring.java
@@ -0,0 +1,46 @@
+package leetcodeDetail;
+
+import java.util.*;
+
+public class LongestSubstring {
+	public static void main(String[] args) {
+		LongestSubstring r = new LongestSubstring();
+		String s = "wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco";
+		System.out.println(r.lengthOfLongestSubstring1(s));
+		System.out.println(r.lengthOfLongestSubstring2(s));
+		
+	}
+	public int lengthOfLongestSubstring1(String s) {
+        char[] cA = s.toCharArray();
+        int[] loc = new int[128];
+        Arrays.fill(loc, -1);
+        int result = 0, start = 0;
+        
+        for (int i = 0; i < cA.length; i++) {
+            if (loc[cA[i] - ' '] >= start) {
+                result = Math.max(result, i - start);
+                start = loc[cA[i] - ' '] + 1;
+            }
+            
+            loc[cA[i] - ' '] = i;
+        }
+        
+        return result = Math.max(result, cA.length - start);
+    }
+	
+	public int lengthOfLongestSubstring2(String s) {
+        char[] cA = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int result = 0, start = 0;
+        
+        for (int i = 0; i < cA.length; i++) {
+            if (map.containsKey(cA[i]) && map.get(cA[i]) >= start) {
+                result = Math.max(result, i - start);
+                start = map.get(cA[i]) + 1;
+            }
+            map.put(cA[i], i);
+        }
+        
+        return result = Math.max(result, cA.length - start);
+    }
+}
